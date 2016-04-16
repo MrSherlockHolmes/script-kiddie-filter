d4972ddf0540c70a1eac1fe6847bea3eb75b2397
Sabri Meeran <MrSherlockHolmes123@gmail.com>
2016-04-16 18:05:12 +0100

    sort of works!:::DDDD

--- script.js
+++ script.js
@@ -2,13 +2,21 @@
 
 Author(s): MrSherlockHolmes
 
+Note: My code is hideous. Please fix it.
+
 */
 
 function init() {
   var focusPosts = document.getElementsByTagName("tbody")[0].getElementsByTagName("tr");
   for (i = 0; i < focusPosts.length; i++) {
-    console.log(focusPosts[i]);
-    // I got bored... someone filter out stickies for meh.
+    var type = focusPosts[i].children[0].children[1].class;
+    if (type !== "isticky" || type !== "iclosed") {
+      // stickies and closed posts are supposedly not to be worried about...
+      if (focusPosts[i].children[0].children[2].getElementsByTagName("a").textContent.toLowerCase().indexOf("tutorial") > -1) {
+        // ouch that is ugly
+        focusPosts[i].children[0].children[2].children[0].style.color = "#bd1e24";
+      }
+    }
   }
 }
 
