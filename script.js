1605469d2e48d3e61b88a1562620c0d614532bb0
scratchyone-test <scratchywon@outlook.com>
2016-04-16 18:40:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,12 +7,21 @@ Note: My code is hideous. Please fix it.
 */
 
 function init() {
+   function contains(val,arr) {
+for(q=0;q<arr.length;q++) {
+if(val.toLowerCase().indexOf(arr[q])>-1) {
+return true;
+}
+}
+return false;
+}
+blocked=["tutorial"];
    var focusPosts = document.getElementsByTagName("tbody")[0].getElementsByTagName("tr");
     for (i = 0; i < focusPosts.length; i++) {
      var type = focusPosts[i].children[0].children[1].class;
      if (type !== "isticky" || type !== "iclosed") {
        // stickies and closed posts are supposedly not to be worried about...
-       if (focusPosts[i].getElementsByTagName("td")[0].getElementsByClassName("tclcon")[0].getElementsByTagName("h3")[0].getElementsByTagName("a")[0].innerHTML.toLowerCase().indexOf("tutorial") > -1) {
+       if (contains(focusPosts[i].getElementsByTagName("td")[0].getElementsByClassName("tclcon")[0].getElementsByTagName("h3")[0].getElementsByTagName("a")[0].innerHTML.toLowerCase(),blocked)) {
          // ouch that is ugly
          focusPosts[i].getElementsByTagName("td")[0].getElementsByClassName("tclcon")[0].getElementsByTagName("h3")[0].getElementsByTagName("a")[0].style.color = "#bd1e24";
        }
